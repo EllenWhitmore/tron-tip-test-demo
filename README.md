713c2d7df59594bb25a922e040e334758b4b4e5e
luoxiongze <aaron.luo@tron.network>
2022-11-23 17:19:28 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+### Quick Stark
+1. run `yarn` to install dependency
+2. run `yarn dev` to start serve
+3. open `http://localhost:9001/` to run demo
+
+### others
+if you want to change test code, open `/public`
+
+
---
+++ webpack.config.version2.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    path: __dirname + '/dist/js',
+    filename: 'index.js'
+  },
+  devServer: {
+    // contentBase: './dist',
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    host: 'localhost',
+    compress: true,
+    port: 9002
+  }
+}
